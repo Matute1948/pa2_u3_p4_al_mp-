5effe66d1733695104aaafa72cb7e3e17a8642af
Matute1948  <achigfelipe@gmail.com>
2023-08-14 19:50:21 -0500

    Taller 38 unidad 4

---
+++ src/test/java/com/uce/pa2_u4_p4_al_mp/Pa2U4P4AlMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.pa2_u4_p4_al_mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U4P4AlMpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
